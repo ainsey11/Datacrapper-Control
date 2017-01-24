7f4f003b88796b8030e23a5bdedf84d9cb44d7b5
Ainsey11 <robert@ainsey11.com>
2017-01-24 23:51:51 +0000

    Moving files around to make some form of methodical structure, creating basic index.html to keep it together for now

--- PowershellExecutor/Pages/PDU/PDUStats.aspx.cs
+++ PowershellExecutor/Pages/PDU/PDUStats.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace PowershellExecutor
+namespace PowershellExecutor.Pages.PDU
 {
-    public partial class About : Page
+    public partial class PDUStats : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- PowershellExecutor/Pages/PDU/PDUStats.aspx.designer.cs
+++ PowershellExecutor/Pages/PDU/PDUStats.aspx.designer.cs
@@ -7,11 +7,20 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PowershellExecutor
+namespace PowershellExecutor.Pages.PDU
 {
 
 
-    public partial class About
+    public partial class PDUStats
     {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
