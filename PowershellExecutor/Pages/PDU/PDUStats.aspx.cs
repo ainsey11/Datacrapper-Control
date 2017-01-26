22df27c4c07e3dbe0722e1bc0c69b31444fa5afe
Ainsey11 <robert@ainsey11.com>
2017-01-26 22:20:23 +0000

    not working yet

--- PowershellExecutor/Pages/PDU/PDUStats.aspx.cs
+++ PowershellExecutor/Pages/PDU/PDUStats.aspx.cs
@@ -13,11 +13,11 @@ namespace PowershellExecutor.Pages.PDU
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            var result = Messenger.Set(VersionCode.V1,
-                                       new IP(("172.16.1.4"), 161),
-                                       new OctetString("public"),
-                                       new List<Variable> { new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.6.0"), new OctetString("Shanghai")) },
-                                       60000);
+            var result = Messenger.Get(VersionCode.V1,
+                                new IPEndPoint(IPAddress.Parse("192.168.1.2"), 161),
+                                new OctetString("public"),
+                                new List<Variable> { new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.1.0")) },
+                                60000);
 
         }
     }
