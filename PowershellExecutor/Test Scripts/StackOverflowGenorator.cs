dc6ce89da114b3e71cc15000631cf24e88ca6945
Ainsey11 <robert@ainsey11.com>
2017-01-27 11:56:42 +0000

    stack overflow genorator added, using as a test for a work problem and being lazy

---
+++ PowershellExecutor/Test Scripts/StackOverflowGenorator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Recursive(int value)
+    {
+        // Write call number and call this method again.
+        // ... The stack will eventually overflow.
+        Console.WriteLine(value);
+        Recursive(++value);
+    }
+
+    static void Main()
+    {
+        // Begin the infinite recursion.
+        Recursive(0);
+    }
+}
