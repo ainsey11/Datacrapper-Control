e1f17f5155b6b82bb224f927750a431fc31077ea
Ainsey11 <robert@ainsey11.com>
2017-01-29 02:48:47 +0000

    Added the start to a generic oid value fetcher

---
+++ PowershellExecutor/Pages/SNMP/OIDValueFetcher.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OIDValueFetcher.aspx.cs" Inherits="PowershellExecutor.Pages.SNMP.OIDValueFetcher" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Datacrapper </title>
+</head>
+<body>
+   <form id="form1" runat="server">
+    <div id="main">
+                <h3>Please Select SNMP Version </h3> 
+                 <asp:RadioButtonList id="IP" runat="server">
+                 <asp:ListItem value="1">1</asp:ListItem>
+                 <asp:ListItem value="2">2</asp:ListItem>
+                 </asp:RadioButtonList>  
+    </div>
+    </form>
+</body>
+</html>
---
+++ PowershellExecutor/Pages/SNMP/OIDValueFetcher.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Management.Automation.Runspaces;
+
+namespace PowershellExecutor.Pages.SNMP
+{
+    public partial class OIDValueFetcher : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void SNMP_ExecCode_OnClick(object sender, EventArgs e)
+        {
+            Runspace snmpoidrunspace = RunspaceFactory.CreateRunspace();
+            snmpoidrunspace.Open();
+            var Shell = PowerShell.Create()
+            .AddCommand(@"C:\Users\Robert\Documents\GitHub\DataCrapper-Control\PowershellExecutor\Powershell\Generic\SNMP\SNMPValueFetcher.ps1")
+            .AddParameter("PDUNumber", IP.SelectedValue);
+            var results = Shell.Invoke();
+
+        }
+    }
+}
