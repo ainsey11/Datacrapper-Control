4252be9ed8d3255e5dddf6c70f21348c9cfde4af
Ainsey11 <robert@ainsey11.com>
2017-01-30 21:17:38 +0000

    Powershell to make a new vm, need to work out guest config somehow

--- PowershellExecutor/Powershell/AutoVM/NewVM.ps1
+++ PowershellExecutor/Powershell/AutoVM/NewVM.ps1
@@ -1,3 +1,16 @@
+param (
+$VmwareServer = "172.16.1.12",
+$VmName,
+$VMCD = $true,
+$VMDatastore,
+$VMDescription,
+$VMDiskMB,
+$VMDiskStorageFormat = "Thin",
+$VMGuestID,
+$VMMemoryMB,
+$VMNumCPU
+)
+
 if ( !(Get-Module -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue) ) {
     if (Test-Path -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VMware, Inc.\VMware vSphere PowerCLI' ) {
         $Regkey = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VMware, Inc.\VMware vSphere PowerCLI'
@@ -11,4 +24,9 @@ if ( !(Get-Module -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue)
     Write-Host "VMware modules not loaded/unable to load"
     Exit 99
 }
+$password = "1A2D5^7*kbD02v"
+Connect-VIServer 172.16.1.12 -User Datacrapper -Password 1A2D5^7*kbD02v $password
 
+
+New-VM -Name $VmName -CD $VMCD -Datastore $VMDatastore -Description $VMDescription -DiskMB $VMDiskMB -DiskStorageFormat $VMDiskStorageFormat -GuestId $VMGuestID -MemoryMB $VMMemoryMB -NumCPU $VMNumCPU 
+return "VM $VMName has been created"
