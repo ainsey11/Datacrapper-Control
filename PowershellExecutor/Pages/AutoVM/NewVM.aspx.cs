7ec07263c58452775a1638fb71c0ba19dccd4d5c
Ainsey11 <robert@ainsey11.com>
2017-02-01 19:04:14 +0000

    meh

--- PowershellExecutor/Pages/AutoVM/NewVM.aspx.cs
+++ PowershellExecutor/Pages/AutoVM/NewVM.aspx.cs
@@ -4,14 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Management.Automation.Runspaces;
 
-namespace PowershellExecutor.Pages.AutoVM
+namespace PowerShellExecution
 {
-    public partial class NewVM : System.Web.UI.Page
+    public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void ExecuteCode_Click(object sender, EventArgs e)
+        {
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+            var Shell = PowerShell.Create()
+            .AddCommand(@"C:\Users\Robert\Documents\GitHub\DataCrapper-Control\PowershellExecutor\Powershell\PDU\PDUControl.ps1")
+            .AddParameter("PDUNumber", PDUNumber.SelectedValue)
+            .AddParameter("PDUOutlet", Input_PDUOutlet.Text)
+            .AddParameter("PDUAction", PDUAction.SelectedValue);
+            var results = Shell.Invoke();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+            Resultbox.Text = stringBuilder.ToString();
+            runspace.Close();
         }
     }
 }
