e5fb036c229cf4391eb1cfbdf959e31da9008f1c
Ainsey11 <robert@ainsey11.com>
2017-02-06 21:04:32 +0000

    add start of sql db works

---
+++ PowershellExecutor/SQL/CreateDBTables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE vmware_power (
+       ID INT IDENTITY(1,1) NOT NULL,
+       PowerUsage Int NOT NULL,
+	   Servername NvarChar(50) NOT NULL,
+       timestamp DATETIME2 NOT NULL,
+       PRIMARY KEY (ID)
+);
+
+Create table vmware_vmstats (
+		ID INT IDENTITY(1,1) NOT NULL,
+		Servername NvarChar(50) NOT NULL,
+		VMname NvarChar(50) NOT NULL,
+		timestamp DATETIME2 NOT NULL,
+		);
+
+Create table network_stats (
+		ID INT IDENTITY(1,1) NOT NULL,
+		Servername NvarChar(50) NOT NULL,
+		Nodename NvarChar(50) NOT NULL,
+		InterfaceName NvarChar(50) NOT NULL,
+		InterfaceIn Int NOT NULL,
+		InterfaceOut Int NOT NULL,
+		timestamp DATETIME2 NOT NULL,
+		);
+
