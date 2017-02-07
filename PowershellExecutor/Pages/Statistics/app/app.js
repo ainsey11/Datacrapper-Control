9735171ec634539b527307815260e1f997ba3185
Ainsey11 <robert@ainsey11.com>
2017-02-07 23:14:12 +0000

    kinda works

--- PowershellExecutor/Pages/Statistics/app/app.js
+++ PowershellExecutor/Pages/Statistics/app/app.js
@@ -1,4 +1,4 @@
 ﻿'use strict';
 
 var app = angular.module('angularServiceDashboard', ['ng.epoch', 'n3-pie-chart']);
-app.value('backendServerUrl', 'http://sitepointsignal.cloudapp.net/');
+app.value('backendServerUrl', 'http://localhost/Pages/Statistics/');
---
+++ PowershellExecutor/Pages/Statistics/app/controllers.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+app.controller('PerformanceDataController', ['$scope', 'backendHubProxy',
+  function ($scope, backendHubProxy) {
+      console.log('trying to connect to service')
+      var performanceDataHub = backendHubProxy(backendHubProxy.defaultServer, 'performanceHub');
+      console.log('connected to service')
+      $scope.currentRamNumber = 68;
+
+      performanceDataHub.on('broadcastPerformance', function (data) {
+          data.forEach(function (dataItem) {
+              switch (dataItem.categoryName) {
+                  case 'Processor':
+                      break;
+                  case 'Memory':
+                      $scope.currentRamNumber = dataItem.value;
+                      break;
+                  case 'Network In':
+                      break;
+                  case 'Network Out':
+                      break;
+                  case 'Disk Read Bytes/Sec':
+                      break;
+                  case 'Disk Write Bytes/Sec':
+                      break;
+                  default:
+                      //default code block
+                      break;
+              }
+          });
+      });
+  }
+]);
